869adae6415dfaf5ed60408deb7ca04dbed9c308
uditmahato <uditmahato29271@gmail.com>
2021-03-15 14:19:23 +0545

    download app section

--- functional.js
+++ functional.js
@@ -1,8 +1,15 @@
-var myCarousel = document.querySelector('#carouselExampleSlidesOnly')
-var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 3000,
-    trasnistion: 2000,
-    wrap: true,
-    touch: true,
-    keyboard: true
+$(document).ready(function() {
+    var scroll_start = 0;
+    var startchange = $('.navbar-default');
+    var offset = startchange.offset();
+
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $('.navbar-default').css('background-color', 'black');
+        } else {
+            $('.navbar-default').css('background-color', 'transparent');
+        }
+    });
+
 });
