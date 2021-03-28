de3a70c90343590dc1eabe9271dc98dde6bbd988
uditmahato <uditmahato29271@gmail.com>
2021-03-28 19:10:17 +0545

    Minor Fixes

--- functional.js
+++ functional.js
@@ -14,3 +14,40 @@ $(document).ready(function() {
 
 });
 //review section js code
+$(document).ready(function() {
+
+    $('.items').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+});
